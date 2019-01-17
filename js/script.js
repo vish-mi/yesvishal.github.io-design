4b77871a27adc9da1779388a3edf263223f43bdf
Vishal Mishra <vishalmishraemailid@gmail.com>
2019-01-17 19:17:51 +0530

    test

---
+++ js/script.js
@@ -0,0 +1,46 @@
+window.addEventListener("load",function(){
+    // setTimeout(function(){
+    //   var load_screen = document.getElementById("loading");
+    //   document.body.removeChild(load_screen);
+    // },3000);
+    var load_screen = document.getElementById("loading");
+    document.body.removeChild(load_screen);
+  });
+
+  jQuery(document).ready(function($) {
+
+    /* testimonial-card*/
+    $.fn.commentCards = function(){
+
+      return this.each(function(){
+    
+        var $this = $(this),
+            $cards = $this.find('.testi-card'),
+            $current = $cards.filter('.card--current'),
+            $next;
+    
+        $cards.on('click',function(){
+          if ( !$current.is(this) ) {
+            $cards.removeClass('card--current card--out card--next');
+            $current.addClass('card--out');
+            $current = $(this).addClass('card--current');
+            $next = $current.next();
+            $next = $next.length ? $next : $cards.first();
+            $next.addClass('card--next');
+          }
+        });
+    
+        if ( !$current.length ) {
+          $current = $cards.last();
+          $cards.first().trigger('click');
+        }
+    
+        $this.addClass('cards--active');
+    
+      })
+    
+    };
+    
+    $('.cards').commentCards();
+    /* testimonial-card*/
+  });
